79ad8bf5834d9290770afa26b2207854202332bb
maniateimoory <maniateimoory3@gmail.com>
2024-11-13 17:10:00 +0330

    commit

---
+++ hidden.js
@@ -0,0 +1,11 @@
+const banner = document.querySelector('.banner');
+
+banner.addEventListener('mousemove' , (e) => {
+    let x = e.pageX - banner.offsetLeft;
+    let y = e.pageY - banner.offsetTop;
+
+    banner.style.setProperty('--x' , x + 'px');
+    banner.style.setProperty('--y' , y + 'px');
+
+
+})
